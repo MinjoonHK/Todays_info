58deab07c78b3d86b81d430f113528898c08655e
MinjoonHK <minjoon.park.hk@gmail.com>
2023-05-28 00:01:57 +0800

    adding api and title images

--- app.js
+++ app.js
@@ -6,42 +6,49 @@ const PORT = 3000;
 app.locals.pretty = true;
 app.set("view engine", "pug"); // template 으로 pug 를 사용
 app.set("views",'./views') //views 라는 폴더를 연결 파일이름은 바뀌어도 됨 하지만 전통적으로 views 폴더
-app.get('/', function(req, res){
-    res.render('index') //send 대신 render 를 사용하면 views 폴더의 temp.pug 로 연결
-})
+// app.get('/', function(req, res){
+//     res.render('index') //send 대신 render 를 사용하면 views 폴더의 temp.pug 로 연결
+// })
 app.use(express.static('public'));
 
-app.get('/topic',function(req,res){
-    // res.send(req.query.id); //id 라는 매개변수를 이용중
-    var topics=[
-        'Javscript?',
-        'Node.js?',
-        'Express.js?',
-    ];
-    var output = `
-        <a href="/topic?id=0">JavaScript</a><br>
-        <a href="/topic?id=1">Node.Js</a><br>
-        <a href="/topic?id=2">Express.Js</a><br>
-        ${topics[req.query.id]}
-    `
-    res.send(output);
-})
 
-
-
-//누군가가 /pet 이라는 경로로 방문을 하면 pet 관련된 안내문을 띄워주자
-
-app.get('/pet',function(req, res){ // /pet 이라는 서버를 만든거임 이제 여기로 html 을 보내면 html이 뜨겟죵?
-    res.send('Welcome to the page');
+app.get('/', (req, res, next) => {
+    Promise.all([
+        fetch('https://api.adviceslip.com/advice').then(response => response.json()),
+        fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=8b60bbeb5da24ed2bf2105332fc9ca3a').then(response => response.json()),
+        fetch('https://raw.githubusercontent.com/KoreanThinker/billboard-json/main/billboard-hot-100/recent.json').then(response => response.json()),
+    ])
+    .then(data_list => {
+        const advice = data_list[0].slip.advice;
+        const news = data_list[1].articles;
+        const music = data_list[2].data;
+        console.log(music);
+        res.render('index', { quotes: advice, news: news, music_list: music });
+    })
+    .catch(error => {
+        console.error(error);
+        next(error);
+    });
 });
 
-app.get('/beauty', function(req,res){
-    res.send('Welcome to the beauty page')
-})
+// app.get('/', (req,res,next) => {
+//     fetch('https://api.adviceslip.com/advice')
+//     .then(response => response.json())
+//     .then(myJson => {
+//         obj = myJson.slip.advice
+//         console.log(obj)
+//         res.render('index',{quotes: obj})
+//         next();
+// })})
 
-app.get('/', function(req,res){ //어떤 사람이 '/' 라는 페이지를 GET 요청하면~ index.html 이라는 파일을 보내주세용
-    res.sendFile(__dirname + '/index.html') //index 에 들어왔을때 이 파일을 보내주세용~
-})
+// app.get('/', function(req,res) {
+//     fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=8b60bbeb5da24ed2bf2105332fc9ca3a')
+//     .then(response => response.json())
+//     .then(myJson => {
+//         obj = myJson.articles
+//         console.log(obj)
+//      res.render('index',{news: obj})
+// })})
 
 app.listen(PORT, function(){
     console.log(`server is running and up at ${PORT} PORT`)
